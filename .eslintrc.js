19a92356c626eebad1d8bb796c09c9f37d6ab955
SYH <you@example.com>
2019-10-29 15:55:44 +0800

    postcss.config.js是针对webpack3.0做的特殊处理

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,14 @@ module.exports = {
     ],
     // add your custom rules here
     'rules': {
+        'eslint eol-last': 0,
+        "indent": ["off", 2],
         // allow paren-less arrow functions
         'arrow-parens': 0,
         // allow async-await
         'generator-star-spacing': 0,
         // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 0 : 0,
         // 忽略函数空格检测
         'space-before-function-paren': 0,
         // 中缀操作符周围要不要有空格
